71d819d101a261443f9edcf5f76679d1f2d3bde9
Najmul Hosain <developer.zeroone@gmail.com>
2023-04-07 15:11:52 +0600

    feat(api): get student grades report implementation

--- api.js
+++ api.js
@@ -1,5 +1,7 @@
+const jsonist = require('jsonist')
 const knex = require('./db')
-const { NotFoundException } = require('./lib/exceptions')
+const { NotFoundException, RequestException } = require('./lib/exceptions')
+const gradesUrl = 'https://outlier-coding-test-data.onrender.com/grades.json'
 
 module.exports = {
   getHealth,
@@ -33,7 +35,25 @@ async function getStudent (req, res, next) {
 }
 
 async function getStudentGradesReport (req, res, next) {
-  throw new Error('This method has not been implemented yet.')
+  try {
+    const studentId = req.params.id;
+
+    const student = await knex('students').where('id', studentId).first();
+    if(!student) {
+      throw new NotFoundException(`student ${studentId} not found`);
+    }
+    const { data, response } = await jsonist.get(gradesUrl)
+    if(response.statusCode !== 200) {
+      throw new RequestException(response.statusCode, res.statusMessage)
+    }
+
+    res.json({
+      ...student,
+      grades: data.filter(grade => grade.id == studentId)
+    })
+  }catch(e){
+    next(e)
+  }
 }
 
 async function getCourseGradesReport (req, res, next) {
